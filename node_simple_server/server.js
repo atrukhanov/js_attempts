06c6d7cc86ae8b749afa5f83344f631494e0ba5d
atrukhanov <team21.alex@gmail.com>
2020-05-06 13:14:56 +0300

    some refactor

--- node_simple_server/server.js
+++ node_simple_server/server.js
@@ -1,33 +1,29 @@
 const express = require('express');
 const http = require('http');
-const app = express();
 const xml2js = require('xml2js');
 
-const options = {
-  hostname: 'http://www.cbr.ru/scripts/XML_daily.asp',
-};
+const app = express();
+const port = 8081;
 
-app.get('/', function (req, app_res) {
-
-   	http.get('http://www.cbr.ru/scripts/XML_daily.asp', (res) => {
-		console.log(`STATUS: ${res.statusCode}`);
-		let str = ' ';
-		res.on('data', (chunk) => {
-    		str += chunk;
-  		});
-  		res.on('end', () => {
-  			xml2js.parseString(str, function (err, result) {
-  				let code = result.ValCurs.Valute[10].CharCode[0];
-  				let value = result.ValCurs.Valute[10].Value[0];
-  				app_res.send('1' + code + '=' + value);
-  			});
-  		});
-   	}); 
-   	
+app.get('/', (req, appRes) => {
+	http.get('http://www.cbr.ru/scripts/XML_daily.asp', httpRes => {
+		console.log(`Status: ${appRes.statusCode}`);
+		let str = '';
+		httpRes.on('data', chunk => {
+			str += chunk;
+		});
+		httpRes.on('end', () => {
+			xml2js.parseString(str, (err, parseRes) => {
+				let code = parseRes.ValCurs.Valute[10].CharCode[0];
+				let value = parseRes.ValCurs.Valute[10].Value[0];
+				appRes.send(`1 ${code} = ${value}`);
+			});
+		});
+	});
 });
 
-let server = app.listen(8081, function () {
+let server = app.listen(port, () => {
 	let host = server.address().address;
 	let port = server.address().port;  
-   	console.log("Example app listening at http://%s:%s", host, port)
+	console.log(`Example app listening at ${host} ${port}`);
 });
