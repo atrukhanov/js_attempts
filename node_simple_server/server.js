491c5c588e86017960674a6f895ecef4744b8ad1
Alex <iampopovich@gmail.com>
2020-05-14 01:12:48 +0300

    code style improvements. minor fixes. gitignor was edited

--- node_simple_server/server.js
+++ node_simple_server/server.js
@@ -10,30 +10,39 @@ const cbLink = 'https://www.cbr.ru/scripts/XML_daily.asp';
 const code = 'R01235';
 
 const getData = (link, callback) => {
-	https.get(link, httpRes => {
+	https.get(link, httpsRes => {
 		let data = '';
-		httpRes.on('data', chunk => {
+		httpsRes.on('data', chunk => {
 			data += chunk;
 		});
-		httpRes.on('end', () => {
-			callback(data);		
+		httpsRes.on('error',err => {
+			console.log(err);
+		});
+		httpsRes.on('end', () => {
+			callback(data);
 		});
 	});
 }
 
 app.get('/', (req, appRes) => {
 	getData(cbLink, data => {
-		let $ = cheerio.load(data, {xmlMode: true});		
-		let node = $(`Valute[ID="${code}"]`);
-		val = `<div>1 ${node.find('CharCode').text()} = ${node.find('Value').text()}</div> 
-		<div><iframe src="${phLink}" scrolling="no" style="overflow:hidden; border:none;">
-		</iframe><div>`;	
-		appRes.send(val);	
+		let $ = cheerio.load(data, {xmlMode: true});
+		let vlt = $(`Valute[ID="${code}"]`);
+		let val = `<div>1${vlt.find('CharCode').text()}</div>
+					<div>${vlt.find('Value').text()}</div>
+					<div>
+						<iframe 
+							src="${phLink}"
+							scrolling="no"
+							style="overflow:hidden; border:none;">
+						</iframe>
+					</div>`;
+		appRes.send(val);
 	});
 });
 
 let server = app.listen(port, () => {
 	let host = server.address().address;
-	let port = server.address().port;  
+	let port = server.address().port;
 	console.log(`Example app listening at ${host} ${port}`);
 });
