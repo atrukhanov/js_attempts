58e4147aeb92ed37b2d0df2d1a353983dcac3502
superheher <heh@vivaldi.net>
2020-08-15 19:11:41 +0300

    Fixed some format of code.

--- node_chat/index.js
+++ node_chat/index.js
@@ -13,19 +13,16 @@ app.use(cookieParser());
 app.use(express.static('frontend'));
 
 app.get('/login', (req, res) => {
-	
-	if (req.query.name){
-		let id = uuid.v4()
+	if (req.query.name) {
+		let id = uuid.v4();
 		CLIENTS.set(id, req.query.name);
 		res.cookie('id', id);
 		res.redirect('chat.html');
-	}
-	else {
+	} else {
 		res.send('login error');
 		res.status(401).redirect('/');
 	}
 });
-
 
 const server = http.createServer(app);
 server.on('upgrade', (req, soc, head) => {
@@ -35,19 +32,19 @@ server.on('upgrade', (req, soc, head) => {
 		return;
 	}
 	wss.handleUpgrade(req, soc, head, (ws) => {
-  		wss.emit('connection', ws, req);
+		wss.emit('connection', ws, req);
 	});
-  });
+});
+
 wss.on('connection', (ws, req) => {
-  let id = cookie.split('=')[1];
-  SOCKETS.set(id, ws);
-  ws.on('message', (data) => {
-    wss.clients.forEach((client) => {
-      if (client.readyState === WebSocket.OPEN) {
-        	client.send(CLIENTS.get(id) + ': ' + data);
-      }
-    });
-  });
+	let id = cookie.split('=')[1];
+	SOCKETS.set(id, ws);
+	ws.on('message', (data) => {
+		wss.clients.forEach((client) => {
+			if (client.readyState === WebSocket.OPEN) {
+				client.send(CLIENTS.get(id) + ': ' + data);
+			}
+		});
+	});
 });
 server.listen(8081);
-
